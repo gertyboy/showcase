581c71dfdcc7a9032b128fdcde64a7eab4b33f0e
Gerrit Thompson <gethompson595@gmail.com>
2026-09-01 20:21:28 -0600

    Added first framework to engine.cpp

---
+++ brew_engine/cpp/engine.cpp
@@ -0,0 +1,18 @@
+// Right now we will only have the main functions as we develop a plan to build this thing
+
+
+// This function will turn the python inputs into readable C++ data structures
+
+
+// This function will take the grind size, water temperature, 
+// yield weight, whether a distribution tool was used, brew time, bean age, 
+
+// This function will take the grinder type and clicks from 0 to give a grind size
+
+// This function will find the type of brew method and return parameters to score 
+// a brew quality
+
+// This funciton will check and validate the inputs to make sure they are within the expected ranges and types
+
+
+
